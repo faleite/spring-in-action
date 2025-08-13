b6d5291d7af65c65f6ec345e1694e5de6302af13
faleite <fabricio_2310@hotmail.com>
2025-08-13 21:28:47 +0100

    (Add) initial Spring Boot project setup with Maven configuration

---
+++ curso-matheus/src/main/java/com/faleite/curso_matheus/CursoMatheusApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.curso_matheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMatheusApplication.class, args);
+	}
+
+}
---
+++ curso-matheus/src/test/java/com/faleite/curso_matheus/CursoMatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.curso_matheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

806c957b3e15d9bf6cafa4746a90c8854c851a04
faleite <fabricio_2310@hotmail.com>
2025-08-16 13:06:19 +0100

    (Add) initial Spring Boot project setup with Maven wrapper, configuration, and basic structure

---
+++ i-cmatheus/src/main/java/com/faleite/i_cmatheus/ICmatheusApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.i_cmatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICmatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICmatheusApplication.class, args);
+	}
+
+}
---
+++ i-cmatheus/src/test/java/com/faleite/i_cmatheus/ICmatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.i_cmatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICmatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

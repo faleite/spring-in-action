bd6fd804a66799dead1d11e82a65b2f791ab78d4
faleite <fabricio_2310@hotmail.com>
2025-08-18 21:29:29 +0100

    first commit for a new project

---
+++ ii-cmatheus/src/main/java/com/faleite/ii_cmatheus/IiCmatheusApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.ii_cmatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiCmatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiCmatheusApplication.class, args);
+	}
+
+}
---
+++ ii-cmatheus/src/test/java/com/faleite/ii_cmatheus/IiCmatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.ii_cmatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiCmatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

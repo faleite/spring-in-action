71b43b132e97e958a24ceb6b628ae44d81543937
faleite <fabricio_2310@hotmail.com>
2025-08-19 20:58:50 +0100

    Add 3rd time project matheus-course

---
+++ iii-cmatheus/src/main/java/com/faleite/iii_cmatheus/IiiCmatheusApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.iii_cmatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiiCmatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiiCmatheusApplication.class, args);
+	}
+
+}
---
+++ iii-cmatheus/src/test/java/com/faleite/iii_cmatheus/IiiCmatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.iii_cmatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiiCmatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

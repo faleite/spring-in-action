e9105ea510208e271335e69d2deeb9f2d3c00f9f
faleite <fabricio_2310@hotmail.com>
2025-08-20 21:13:37 +0100

    (Update) Init one more time

---
+++ iv-cmatheus/src/main/java/com/faleite/iv_cmatheus/IvCmatheusApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.iv_cmatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvCmatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvCmatheusApplication.class, args);
+	}
+
+}
---
+++ iv-cmatheus/src/test/java/com/faleite/iv_cmatheus/IvCmatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.iv_cmatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvCmatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4d4e12e770522964285d70d9875d54ade044f91d
faleite <fabricio_2310@hotmail.com>
2025-08-21 21:17:05 +0100

    Add one more cmatheus project for learning

---
+++ v-cmatheus/src/main/java/com/faleite/v_cmatheus/VCmatheusApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.v_cmatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VCmatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VCmatheusApplication.class, args);
+	}
+
+}
---
+++ v-cmatheus/src/test/java/com/faleite/v_cmatheus/VCmatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.v_cmatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VCmatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

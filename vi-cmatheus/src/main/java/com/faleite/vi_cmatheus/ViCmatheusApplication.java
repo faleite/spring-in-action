deeec96483f061c873215ee6ecb1922547323bb6
faleite <fabricio_2310@hotmail.com>
2025-08-22 20:35:41 +0100

    Add one more project by matheus basic course

---
+++ vi-cmatheus/src/main/java/com/faleite/vi_cmatheus/ViCmatheusApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.vi_cmatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViCmatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViCmatheusApplication.class, args);
+	}
+
+}

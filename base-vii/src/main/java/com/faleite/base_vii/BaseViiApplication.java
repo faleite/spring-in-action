3ad755696c0210e78a2d001cb6bd3f1733e9b14a
faleite <fabricio_2310@hotmail.com>
2025-10-18 15:28:01 +0100

    (Add) Initialize Spring Boot project with Maven support, basic configuration files, and PostgreSQL dependencies

---
+++ base-vii/src/main/java/com/faleite/base_vii/BaseViiApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.base_vii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseViiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseViiApplication.class, args);
+	}
+
+}
---
+++ base-vii/src/test/java/com/faleite/base_vii/BaseViiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.base_vii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseViiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
